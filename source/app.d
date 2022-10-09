19cbb06772802904aaa4eb43ae63378d66296fd0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-09 04:56:40 -0400

    Get real fancy with the drawing

--- source/app.d
+++ source/app.d
@@ -7,7 +7,7 @@ void main() {
 
     InitWindow(512,512, "2D Game");
 
-    Camera2D camera = Camera2D(Vector2(0,0), Vector2(0,0), 0, 1);
+    Camera2D camera = Camera2D(Vector2(0,0), Vector2(0,0), 0, 0.25);
 
     Texture blah = LoadTexture("textures/modernCity/0.png");
 
@@ -19,12 +19,12 @@ void main() {
             ClearBackground(Colors.BLACK);
 
             foreach (x; 0..32) {
-                foreach (y; 0..32) {
-                    DrawTexture(blah,x * 16, y * 16, Colors.RAYWHITE);
+                foreach (y; 0..32) {                    
+                    // DrawTexture(blah,x * 16, y * 16, Colors.RAYWHITE);
+                    DrawTextureEx(blah,Vector2(x * 32, y * 32), 0, 2, Colors.WHITE);
                 }
             }
 
-            // DrawTextureEx(blah,Vector2(0,0), 0, 200, Colors.WHITE);
 
             
         }
