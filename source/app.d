a85e074c055e95d1c1aa95417ca9b0c42d6e7a66
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-09 19:01:55 -0400

    Begin base implementation

--- source/app.d
+++ source/app.d
@@ -2,6 +2,40 @@ import std.stdio;
 
 import raylib;
 
+/// World is what the game takes place in
+public class World {
+    Map map;
+
+}
+
+/// The world has a map
+public class Map {
+    int width;
+    int height;
+
+    final
+    float get(int x, int y) {
+        if (x < 0 || x > this.width - 1) {
+            throw new Exception("X getter is out of bounds for heightmap!");
+        }
+        if (y < 0 || y > this.height - 1) {
+            throw new Exception("Y getter is out of bounds for heightmap!");
+        }
+        return heightMap[(x * this.heightMapSize) + y];
+    }
+
+}
+
+/// The map has map tiles
+public class MapTile {
+
+}
+
+/// The map tiles have layers
+public class Layer {
+    
+}
+
 void main() {
 	validateRaylibBinding();
 
@@ -18,10 +52,11 @@ void main() {
         {
             ClearBackground(Colors.BLACK);
 
-            foreach (x; 0..32) {
-                foreach (y; 0..32) {                    
+            foreach (float x; 0..32) {
+                foreach (float y; 0..32) {
+                    writefln("%f, %f", x, y);
                     // DrawTexture(blah,x * 16, y * 16, Colors.RAYWHITE);
-                    DrawTextureEx(blah,Vector2(x * 32, y * 32), 0, 2, Colors.WHITE);
+                    // DrawTextureEx(blah,Vector2(x * 32, y * 32), 0, 2, Colors.WHITE);
                 }
             }
 
