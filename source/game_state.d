8e2b7adcbd278dff92728757d27ea98f6f735d45
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-13 20:37:21 -0400

    Experiment with internal redirection

--- source/game_state.d
+++ source/game_state.d
@@ -2,14 +2,30 @@ module game_state;
 
 import game;
 import std.stdio;
+import world;
+import window;
+import camera;
+import texture;
+import time_keeper;
+import mouse;
 
 public class GameState {
 
-    Game game;
+    World world;
+    Window window;
+    Cam camera;
+    TextureCache cache;
+    TimeKeeper timeKeeper;
+    Mouse mouse;
 
     // Assign pointer for all GameStates
     this(Game game) {
-        this.game = game;
+        world  = game.world;
+        window = game.window;
+        camera = game.camera;
+        cache  = game.cache;
+        timeKeeper = game.timeKeeper;
+        mouse = game.mouse;
     }
     void start() {
         throw new Exception("Start() is not implimented for a GameState!");
