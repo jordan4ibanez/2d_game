fdc1f9dc8b7a6cbc21cdd05dc1c421f9abb6a885
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-13 22:04:14 -0400

    Create a very usable keyboard basis

---
+++ source/keyboard.d
@@ -0,0 +1,42 @@
+module keyboard;
+
+import raylib;
+import std.stdio;
+import std.uni: toLower;
+import std.string: strip;
+import std.conv: to;
+
+alias Key = KeyboardKey;
+
+public class Keyboard {
+    
+    int[string] keys;
+    bool[string] pressed;
+    bool[string] down;
+
+    this() {
+        Key[] newKeys = [
+            Key.KEY_LEFT_CONTROL,
+            Key.KEY_LEFT_SHIFT
+        ];
+        insertKeys(newKeys);
+    }
+
+    private
+    void insertKeys(KeyboardKey[] newKeys) {
+        foreach (thisKey; newKeys) {
+            string stringKey = strip(to!string(thisKey).toLower, "key_");
+            keys[stringKey] = thisKey;
+            pressed[stringKey] = false;
+            down[stringKey] = false;
+        }
+    }
+
+    bool isDown(string keyName) {
+        return down[keyName];
+    }
+
+    bool isPressed(string keyName) {
+        return pressed[keyName];
+    }
+}
