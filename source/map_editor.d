30da5ce0067188ca52a528d9ad3576a45995cf61
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-13 21:23:06 -0400

    Fancier map_editor base

--- source/map_editor.d
+++ source/map_editor.d
@@ -4,6 +4,7 @@ import std.stdio;
 import game_state;
 import game;
 import raylib;
+import world;
 
 
 public class MapEditor : GameState {
@@ -11,14 +12,27 @@ public class MapEditor : GameState {
     int size = 10;
     bool up = true;
 
+    /*
+    modes:
+    0 - move
+    1 - add
+    2 - remove
+    */
+    int mode = 0;
+
+    World world;
+    
     this(Game game) {
         super(game);
+        this.world = new World(50, 50);
     }
     
     override
     void start() {
+        camera.setClearColor(0,0,0,0);
 
     }
+
 
     override
     void update() {
@@ -31,9 +45,6 @@ public class MapEditor : GameState {
             Vector2 mouseDelta = mouse.getDelta();
             camera.addOffset(mouseDelta);
         }
-
-        // writeln(zoom);
-
     }
 
     override
@@ -56,15 +67,36 @@ public class MapEditor : GameState {
             }
             */
 
-
-            foreach (x; 0..37) {
-                foreach (y; 0..28) {
-                    DrawRectangleLines(x * 16, y * 16, 16, 16, Colors.BLACK);
+            foreach (x; 0..world.map.width) {
+                foreach (y; 0..world.map.height) {
+                    if (world.map.get(x,y) is null) {
+                        DrawRectangleLines(x * 16, y * 16, 16, 16, Colors.BLACK);
+                    }
                     // world.drawTile(x,y,x,y,1);
                 }
             }
         }
         EndMode2D();
+
+        final switch (mode) {
+            case 0: {
+                DrawText("MODE: MOVE", 4,2, 38, Colors.BLACK);
+                DrawText("MODE: MOVE", 2,0, 38, Color(57, 255, 20, 255));
+                break;
+            }
+            case 1: {
+                DrawText("MODE: PLACE", 4,2, 38, Colors.BLACK);
+                DrawText("MODE: PLACE", 2,0, 38, Color(57, 255, 20, 255));
+                break;
+            }
+
+            case 2: {
+                DrawText("MODE: REMOVE", 4,2, 38, Colors.BLACK);
+                DrawText("MODE: REMOVE", 2,0, 38, Color(57, 255, 20, 255));
+                break;
+            }
+
+        }
         EndDrawing();
     }
 
