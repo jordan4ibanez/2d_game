a51e7902d7d50d9fff1d2f572ff75202dbe625c0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-13 20:12:44 -0400

    Re-engineer gamestate

--- source/game_state.d
+++ source/game_state.d
@@ -1,7 +1,29 @@
 module game_state;
 
-public interface GameState {
-    void start();
-    void update();
-    void render();
+import game;
+import std.stdio;
+
+public class GameState {
+
+    Game game;
+
+    // Assign pointer for all GameStates
+    this(Game game) {
+        this.game = game;
+    }
+    void start() {
+        throw new Exception("Start() is not implimented for a GameState!");
+    }
+    void update() {
+        throw new Exception("Update() is not implimented for a GameState!");
+    }
+    void render() {
+        throw new Exception("Render() is not implimented for a GameState!");
+    }
+    void reset() {
+        throw new Exception("Reset() is not implimented for a GameState!");
+    }
+    void cleanUp() {
+        throw new Exception("CleanUp() is not implimented for a GameState!");
+    }
 }
