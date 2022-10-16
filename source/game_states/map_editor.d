f4444fefcd4118bfef58936bd5e2b05ba4a024fb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-15 20:28:57 -0400

    Start squigglin

--- source/game_states/map_editor.d
+++ source/game_states/map_editor.d
@@ -69,6 +69,9 @@ public class MapEditor : GameState {
         if (keyboard.isPressed("f5")) {
             exporter.flushToDisk();
         }
+        if (keyboard.isPressed("grave")) {
+            writeln("squigglin!");
+        }
 
         if (atlasBrowserMode) {
 
