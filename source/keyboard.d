bd3605667dde4cdc47c0fb181603abe0c71a6c78
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-15 23:23:00 -0400

    Ultimate GLFW flexibility

--- source/keyboard.d
+++ source/keyboard.d
@@ -5,6 +5,7 @@ import std.stdio: writeln;
 import std.uni: toLower;
 import std.conv: to;
 import std.array: replace;
+import std.traits: EnumMembers;
 
 alias Key = KeyboardKey;
 
@@ -18,28 +19,13 @@ public class Keyboard {
 
     this() {
         // string interface becomes left_control, left_shift, etc
-        Key[] newKeys = [
-            Key.KEY_LEFT_CONTROL,
-            Key.KEY_LEFT_SHIFT,
-            Key.KEY_TAB,
-            Key.KEY_F5,
-            Key.KEY_GRAVE, // Squiggly boi "~"
-            Key.KEY_EQUAL,
-            Key.KEY_MINUS
-        ];
-        insertKeys(newKeys);
-    }
-
-    private
-    void insertKeys(KeyboardKey[] newKeys) {
-        foreach (thisKey; newKeys) {
+        foreach (thisKey; EnumMembers!Key){
             string stringKey = to!string(thisKey).toLower.replace("key_", "");
             this.keys[stringKey] = thisKey;
             this[stringKey ~ "_down"] = false;
             this[stringKey ~ "_pressed"] = false;
         }
     }
-
     @property
     bool opDispatch(string name)() {
         return values[name];
