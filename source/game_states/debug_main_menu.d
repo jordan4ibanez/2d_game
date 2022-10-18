535408139a309c15f78fb24b04f34b441d086638
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-18 17:28:39 -0400

    Impl overrides

--- source/game_states/debug_main_menu.d
+++ source/game_states/debug_main_menu.d
@@ -1,8 +1,39 @@
 module game_states.debug_main_menu;
 
 import game_states.game_state;
+import game;
+import utility.gui;
 
 
 public class MainMenu : GameState {
-    
+
+    this(Game game) {
+        super(game);
+    }
+
+    override
+    void start() {
+        
+    }
+
+    override
+    void update() {
+        
+    }
+
+    override
+    void render() {
+        
+    }
+
+    override
+    void reset() {
+        
+    }
+
+    override
+    void cleanUp() {
+        
+    }
+
 }
