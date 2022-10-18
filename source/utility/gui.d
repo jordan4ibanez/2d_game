efc36d5411ce501a450c9cf50cb7a564b13d2b02
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-17 23:17:03 -0400

    Blindly add GUI elements

--- source/utility/gui.d
+++ source/utility/gui.d
@@ -1,8 +1,23 @@
 module utility.gui;
 
+import raylib;
+
+//! I've never made a generic GUI before so this might be a disaster
 public class GUI {
 
+
+    //! Literally no idea what to put here
+    static enum Type {
+        TEXT,
+        INPUT,
+        BUTTON,
+        ANIMATED,
+        DROP_MENU
+    }
+
+    float scale;
+    
     GUI[string] nodes;
 
-    
+
 }
