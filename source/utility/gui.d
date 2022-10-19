9dc18925c305c29c147d57300d7109d5819ef1df
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-19 17:57:03 -0400

    GUI is now modular

--- source/utility/gui.d
+++ source/utility/gui.d
@@ -25,23 +25,22 @@ static immutable enum Anchor {
     CENTER = Vector2(0.5 , 0.5)
 }
 
+static immutable enum ElementType {
+    NULL,
+    TEXT,
+    ANIMATED_TEXT,
+    INPUT,
+    BUTTON,
+    IMAGE,
+    DROP_MENU,
+    // Windows are going to be real fun to debug
+    WINDOW
+}
+
+
 //! I've never made a generic GUI before so this might be a disaster
 public class GUI {
-
-
-    //! Literally no idea what to put here
-    static immutable enum ElementType {
-        NULL,
-        TEXT,
-        ANIMATED_TEXT,
-        INPUT,
-        BUTTON,
-        IMAGE,
-        DROP_MENU,
-        // Windows are going to be real fun to debug
-        WINDOW
-    }
-
+    
     int windowWidth;
     int windowHeight;
 
@@ -55,6 +54,28 @@ public class GUI {
         this.window = window;
     }
 
+    //! Text Elements
+    void addTextElement(Anchor anchor, string ID, string text, int offsetX, int offsetY, int fontSize, Color color) {
+        this.textElements[ID] = new GUIText(anchor, offsetX,offsetY, text, fontSize, color);
+    }
+    void removeTextElement(string ID) {
+        this.textElements.remove(ID);
+    }
+    GUIText getTextElement(string ID) {
+        return this.textElements[ID];
+    }
+
+    //! Image Elements
+    void addImageElement(Anchor anchor, string ID, int offsetX, int offsetY, Texture texture, float scale) {
+        this.imageElements[ID] = new GUIImage(anchor, offsetX,offsetY, texture, scale);
+    }
+    void removeImageElement(string ID) {
+        this.imageElements.remove(ID);
+    }
+    GUIImage getImageElement(string ID) {
+        return this.imageElements[ID];
+    }
+
     void render() {
 
         Vector2 wSize = window.getSize();
@@ -62,187 +83,159 @@ public class GUI {
         windowHeight = cast(int)wSize.y;
 
         foreach (element; textElements) {
+            element.updateParams(windowWidth, windowHeight);
             element.render();
         }
 
         foreach (element; imageElements) {
+            element.updateParams(windowWidth, windowHeight);
             element.render();            
         }
     }
+}
 
-    private class GUIElement {
+private class GUIElement {
 
-        ElementType elementType = ElementType.NULL;
+    ElementType elementType = ElementType.NULL;
 
-        Anchor anchor = Anchor.TOP_LEFT;
+    Anchor anchor = Anchor.TOP_LEFT;
 
-        private float scale;
-        private Vector2 size;
-        private Vector2 offset;
+    private float scale;
+    private Vector2 size;
+    private Vector2 offset;
 
-        // ? holds a pointer or what? it's on the heap anyways
-        private Texture texture;
+    int windowHeight;
+    int windowWidth;
 
-        private bool visible = true;
+    private bool visible = true;
 
-        Texture getTexture() {
-            return texture;
-        }
-
-        void setTexture(Texture texture) {
-            this.texture = texture;
-        }
-
-        Vector2 getOffset() {
-            return offset;
-        }
-
-        void setOffset(Vector2 offset) {
-            this.offset = offset;
-        }
-
-        Vector2 getSize() {
-            return size;
-        }
-
-        void setSize(Vector2 size) {
-            this.size = size;
-        }
-
-        float getScale() {
-            return scale;
-        }        
-
-        void setScale(float scale) {
-            this.scale = scale;
-        }
-
-        bool isVisible() {
-            return visible;
-        }
-
-        void setVisible(bool visibility) {
-            this.visible = visibility;
-        }
-
-        void render() {
-            throw new Exception("Render is not implemented for this element!");
-        }
+    void updateParams(int windowWidth, int windowHeight) {
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
     }
 
-
-
-
-    void addTextElement(Anchor anchor, string ID, string text, int offsetX, int offsetY, int fontSize, Color color) {
-        this.textElements[ID] = new GUIText(anchor, offsetX,offsetY, text, fontSize, color);
+    Vector2 getOffset() {
+        return offset;
     }
 
-    void removeTextElement(string ID) {
-        this.textElements.remove(ID);
+    void setOffset(Vector2 offset) {
+        this.offset = offset;
     }
 
-    GUIText getTextElement(string ID) {
-        return this.textElements[ID];
+    Vector2 getSize() {
+        return size;
     }
 
-    public class GUIText : GUIElement {
-
-        //! Now with more words!
-        private string text;
-        private bool shadowed;
-        private int fontSize;
-        private int length;
-        private Vector2 textSize;
-        float spacing;
-        Color color;
-
-        this(Anchor anchor, int offsetX, int offsetY, string text, int fontSize, Color color) {
-            this.text = text;
-            this.elementType = ElementType.TEXT;
-            this.anchor = anchor;
-            this.fontSize = fontSize;
-            this.length = cast(int)text.length;
-            this.offset = Vector2(offsetX, offsetY);
-            this.spacing = fontSize/GetFontDefault().baseSize;
-            this.textSize = MeasureTextEx(GetFontDefault(),toStringz(text), fontSize, spacing);
-            this.color = color;
-        }
-
-        string getText() {
-            return text;
-        }
-
-        void setText(string text) {
-            this.text = text;
-            this.length = cast(int)text.length;
-            this.spacing = fontSize/GetFontDefault().baseSize;
-            this.textSize = MeasureTextEx(GetFontDefault(),toStringz(text), fontSize, spacing);
-        }
-
-        int getFontSize() {
-            return fontSize;
-        }
-
-        void setFontSize(int fontSize) {
-            this.fontSize = fontSize;
-            this.spacing = fontSize/GetFontDefault().baseSize;
-            this.textSize = MeasureTextEx(GetFontDefault(),toStringz(text), fontSize, spacing);
-        }
-
-        override
-        void render() {            
-
-            float positionRenderX = ((anchor.x * windowWidth)  - (anchor.x * textSize.x)) + offset.x;
-            float positionRenderY = ((anchor.y * windowHeight) - (anchor.y * textSize.y)) + offset.y;
-
-            DrawText(
-                toStringz(text),
-                cast(int)positionRenderX,
-                cast(int)positionRenderY,
-                fontSize,
-                color
-            );
-        }
+    void setSize(Vector2 size) {
+        this.size = size;
     }
 
-    void addImageElement(Anchor anchor, string ID, int offsetX, int offsetY, Texture texture, float scale) {
-        this.imageElements[ID] = new GUIImage(anchor, offsetX,offsetY, texture, scale);
+    float getScale() {
+        return scale;
+    }        
+
+    void setScale(float scale) {
+        this.scale = scale;
     }
 
-    void removeImageElement(string ID) {
-        this.imageElements.remove(ID);
+    bool isVisible() {
+        return visible;
     }
 
-    GUIImage getImageElement(string ID) {
-        return this.imageElements[ID];
+    void setVisible(bool visibility) {
+        this.visible = visibility;
     }
 
-    public class GUIImage : GUIElement {
-
-        private Texture texture;
-
-        float width;
-        float height;
-        float scale;
-
-        this(Anchor anchor, int offsetX, int offsetY, Texture texture, float scale) {
-            this.elementType = ElementType.IMAGE;
-            this.anchor = anchor;
-            this.offset = Vector2(offsetX, offsetY);
-            this.texture = texture;
-            this.scale = scale;
-            width = texture.width;
-            height = texture.height;
-        }
-
-        override
-        void render() {
-
-            float positionRenderX = ((anchor.x * windowWidth)  - (anchor.x * width * scale)) + offset.x;
-            float positionRenderY = ((anchor.y * windowHeight) - (anchor.y * height * scale)) + offset.y;
-            
-            DrawTextureEx(texture, Vector2(positionRenderX, positionRenderY), 0, scale, Colors.WHITE);            
-        }
-
+    void render() {
+        throw new Exception("Render is not implemented for this element!");
     }
+}
+
+public class GUIText : GUIElement {
+
+    //! Now with more words!
+    private string text;
+    private bool shadowed;
+    private int fontSize;
+    private int length;
+    private Vector2 textSize;
+    float spacing;
+    Color color;
+
+    this(Anchor anchor, int offsetX, int offsetY, string text, int fontSize, Color color) {
+        this.text = text;
+        this.elementType = ElementType.TEXT;
+        this.anchor = anchor;
+        this.fontSize = fontSize;
+        this.length = cast(int)text.length;
+        this.offset = Vector2(offsetX, offsetY);
+        this.spacing = fontSize/GetFontDefault().baseSize;
+        this.textSize = MeasureTextEx(GetFontDefault(),toStringz(text), fontSize, spacing);
+        this.color = color;
+    }
+
+    string getText() {
+        return text;
+    }
+
+    void setText(string text) {
+        this.text = text;
+        this.length = cast(int)text.length;
+        this.spacing = fontSize/GetFontDefault().baseSize;
+        this.textSize = MeasureTextEx(GetFontDefault(),toStringz(text), fontSize, spacing);
+    }
+
+    int getFontSize() {
+        return fontSize;
+    }
+
+    void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+        this.spacing = fontSize/GetFontDefault().baseSize;
+        this.textSize = MeasureTextEx(GetFontDefault(),toStringz(text), fontSize, spacing);
+    }
+
+    override
+    void render() {            
+
+        float positionRenderX = ((anchor.x * windowWidth)  - (anchor.x * textSize.x)) + offset.x;
+        float positionRenderY = ((anchor.y * windowHeight) - (anchor.y * textSize.y)) + offset.y;
+
+        DrawText(
+            toStringz(text),
+            cast(int)positionRenderX,
+            cast(int)positionRenderY,
+            fontSize,
+            color
+        );
+    }
+}
+
+public class GUIImage : GUIElement {
+
+    private Texture texture;
+
+    float width;
+    float height;
+    float scale;
+
+    this(Anchor anchor, int offsetX, int offsetY, Texture texture, float scale) {
+        this.elementType = ElementType.IMAGE;
+        this.anchor = anchor;
+        this.offset = Vector2(offsetX, offsetY);
+        this.texture = texture;
+        this.scale = scale;
+        width = texture.width;
+        height = texture.height;
+    }
+
+    override
+    void render() {
+
+        float positionRenderX = ((anchor.x * windowWidth)  - (anchor.x * width * scale)) + offset.x;
+        float positionRenderY = ((anchor.y * windowHeight) - (anchor.y * height * scale)) + offset.y;
+        
+        DrawTextureEx(texture, Vector2(positionRenderX, positionRenderY), 0, scale, Colors.WHITE);            
+    }
+
 }
