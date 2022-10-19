0bdda5ee76362619591aed4de385e15aaedf0742
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-18 22:09:18 -0400

    Add a horrific debug test

--- source/game_states/debug_main_menu.d
+++ source/game_states/debug_main_menu.d
@@ -4,28 +4,41 @@ import raylib;
 import game_states.game_state;
 import game;
 import utility.gui;
+import std.random;
 
 
 public class MainMenu : GameState {
 
     GUI gui;
 
+    string[9] runners = [
+        "debug1",
+        "debug2",
+        "debug3",
+        "debug4",
+        "debug5",
+        "debug6",
+        "debug7",
+        "debug8",
+        "debug9",
+    ];
+
     this(Game game) {
         super(game);
 
         gui = new GUI(window);
-        
-        gui.addText(Anchor.TOP_LEFT,     "debug1", "my test",0,0,20);
-        gui.addText(Anchor.TOP,          "debug2", "my test",0,0,20);
-        gui.addText(Anchor.TOP_RIGHT,    "debug3", "my test",0,0,20);
 
-        gui.addText(Anchor.BOTTOM_LEFT,  "debug4", "my test",0,0,20);
-        gui.addText(Anchor.BOTTOM,       "debug5", "my test",0,0,20);
-        gui.addText(Anchor.BOTTOM_RIGHT, "debug6", "my test",0,0,20);
+        gui.addTextElement(Anchor.TOP_LEFT,     "debug1", "my test",0,0,20);
+        gui.addTextElement(Anchor.TOP,          "debug2", "my test",0,0,20);
+        gui.addTextElement(Anchor.TOP_RIGHT,    "debug3", "my test",0,0,20);
 
-        gui.addText(Anchor.LEFT,         "debug7", "my test",0,0,20);
-        gui.addText(Anchor.CENTER,       "debug9", "my test",0,0,20);
-        gui.addText(Anchor.RIGHT,        "debug8", "my test",0,0,20);
+        gui.addTextElement(Anchor.BOTTOM_LEFT,  "debug4", "my test",0,0,20);
+        gui.addTextElement(Anchor.BOTTOM,       "debug5", "my test",0,0,20);
+        gui.addTextElement(Anchor.BOTTOM_RIGHT, "debug6", "my test",0,0,20);
+
+        gui.addTextElement(Anchor.LEFT,         "debug7", "my test",0,0,20);
+        gui.addTextElement(Anchor.CENTER,       "debug9", "my test",0,0,20);
+        gui.addTextElement(Anchor.RIGHT,        "debug8", "my test",0,0,20);
 
     }
 
@@ -38,7 +51,19 @@ public class MainMenu : GameState {
 
     override
     void update() {
-        
+        foreach (key; runners) {
+
+            string blorf;
+
+            Random randy = Random(unpredictableSeed());
+            foreach (_; 0..uniform(5,15, randy)) {
+                randy = Random(unpredictableSeed());
+                blorf ~= cast(char)uniform(0, 127, randy);
+            }
+
+            gui.getTextElement(key).setText(blorf);
+            
+        }
     }
 
     override
