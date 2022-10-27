f4be07c3fd1f0c8d6b192e8945bb5043a8a804ad
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-10-26 20:16:56 -0400

    Update debug_main_menu.d

--- source/game_states/debug_main_menu.d
+++ source/game_states/debug_main_menu.d
@@ -44,115 +44,10 @@ public class MainMenu : GameState {
 
         Color pumpkinOrange = Color(255, 117, 24,255);
 
-        // Constructor function
-        void init(GUITextAnimated animation) {
-            animation.singularIntMemory = 1;
-            animation.singularBoolMemory = true;
-            animation.singularFloatMemory = 0;
-        }
-
-        // Bouncing text - treating the entire object as the animation
-        void update(GUITextAnimated animation, float delta) {                
-            string text = animation.getText();
-            int textLength = cast(int) text.length;
-
-            bool  goUp        = animation.singularBoolMemory;
-            float height      = animation.singularFloatMemory;
-            int   stage       = animation.singularIntMemory;
-
-            static immutable float bounceAmount = 7;
-            static immutable float bounceSpeed = 50;
-
-            if (goUp) {
-                height += delta * bounceSpeed;
-                if (height >= bounceAmount) {
-                    stage += 1;
-                    goUp = false;
-                    height = bounceAmount;
-                }
-            } else {
-                height -= delta * bounceSpeed;
-                if (height <= 0) {
-                    stage += 1;
-                    goUp = true;
-                    height = 0;
-                }
-            }
-            if (stage > 4) {
-                stage = 1;
-            }
-
-
-            bool odd = true;
-            foreach (i; 0..textLength){
-                if (stage < 3) {
-                    if (odd) {                            
-                        animation.offsetMemory[i].y = height;
-                    }
-                } else {
-                    if (!odd) {                            
-                        animation.offsetMemory[i].y = height;
-                    }
-                }
-                odd = !odd;
-            }
-
-            animation.singularBoolMemory  = goUp;
-            animation.singularFloatMemory = height;
-            animation.singularIntMemory  = stage;
-
-        }
         
-        gui.addAnimatedTextElement("HAPPY",     Anchor.CENTER, "HAPPY",     0, -150, 50, Colors.BLACK, false, &init, &update);
-        gui.addAnimatedTextElement("HALLOWEEN", Anchor.CENTER, "HALLOWEEN", 0,  150, 50, Colors.BLACK, false, &init, &update);
-        gui.getAnimatedTextElement("HAPPY").setVisible(false);
-        gui.getAnimatedTextElement("HALLOWEEN").setVisible(false);
+        
         
         gui.addTextElement("question", Anchor.CENTER, "What is your name?", 0, -50, 30, Colors.WHITE, true);
-        
-        // Sass is eternal
-        static immutable string[] sass = [
-            "What is your name?",
-            "Surely, you must have a name!",
-            "Oh come on, type your name..",
-            "I don't have all day here, type that name!",
-            "Name! Now!",
-            "Okay fine, don't type your name",
-            "See if I care!",
-            "I'm getting tired of this",
-            "I bet your name is Bob. Hi Bob.",
-            "That's it, I'm resetting back to the first index."
-        ];
-
-        gui.addTextInputElement("namePrompt", "", "*input name here*", Anchor.CENTER, 0, 0, 60, 350, 30, 3, Colors.WHITE, Colors.GRAY, Colors.BLACK, Colors.WHITE,
-            (GUIInput textInput) {
-
-                if (textInput.getText().length == 0) {
-                    sassIndex++;
-                    if (sassIndex >= sass.length) {
-                        sassIndex = 0;
-                    }
-                    gui.getTextElement("question").setText(sass[sassIndex]);
-                } else {
-                    gui.getTextElement("question").setText("What is your name?");
-                    gui.getAnimatedTextElement("HAPPY").setVisible(true);
-                    GUIText greeting =  gui.getAnimatedTextElement("HALLOWEEN");
-                    greeting.setText("HALLOWEEN " ~ textInput.getText() ~ "!");
-                    greeting.setVisible(true);
-
-                    gui.getImageElement("pumpkin").setVisible(true);
-
-                    gui.getTextElement("question").setVisible(false);
-                    textInput.setVisible(false);
-
-                    camera.setClearColor(255, 117, 24,255);
-
-                    musicPlaying = true;
-                }
-
-            }
-        );
-        // gui.addTextInputElement("overlapText", "", "Focused?", Anchor.RIGHT, 0, 20, 10, 220, 20, 3, Colors.WHITE, Colors.GRAY, Colors.BLACK, Colors.WHITE);
 
 
         cache.upload("jackolantern", "textures/jackolantern.png");
@@ -161,13 +56,13 @@ public class MainMenu : GameState {
          
         gui.addImageElement("pumpkin", Anchor.CENTER, 10,0, pumpkin, 0.75);
 
-        gui.getImageElement("pumpkin").setVisible(false);
+        // gui.getImageElement("pumpkin").setVisible(false);
 
     }
 
     override
     void start() {
-        camera.setClearColor(90,90,90,255);
+        camera.setClearColor(255, 117, 24,255);
 
         camera.setOffset(Vector2(0,0));
 
@@ -185,29 +80,6 @@ public class MainMenu : GameState {
             UpdateMusicStream(music);
         }
 
-        if (keyboard.f1_pressed) {
-
-            gui.getAnimatedTextElement("HAPPY").setVisible(false);
-
-            gui.getAnimatedTextElement("HALLOWEEN").setVisible(false);
-
-            gui.getImageElement("pumpkin").setVisible(false);
-
-            gui.getTextElement("question").setVisible(true);
-
-            GUIInput blah =  gui.getTextInputElement("namePrompt");
-            blah.setText("");
-            blah.focused = false; // oops forgot this one
-            blah.setVisible(true);
-
-            camera.setClearColor(90, 90, 90,255);
-
-            musicPlaying = false;
-            SeekMusicStream(music, 0);
-            
-            sassIndex = 0;
-            
-        }
 
         /*
         float delta = timeKeeper.getDelta();
